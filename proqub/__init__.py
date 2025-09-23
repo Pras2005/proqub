d8c6e6921b355da7c4f71a209d13b52ad838dc6a
pras2005 <prasaddesale2005@gmail.com>
2025-09-23 18:30:08 +0530

    intial commit

--- proqub/__init__.py
+++ proqub/__init__.py
@@ -5,6 +5,6 @@ This package provides tools for common hyperspectral processing tasks
 like radiance-to-reflectance conversion and destriping in a memory-efficient pipeline.
 """
 
-from .processor import CubeProcessor, run_pipeline
+from .processor import CubeProcessor
 
 __version__ = "1.0.0"
